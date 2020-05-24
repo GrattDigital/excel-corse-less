3f8cab55bd2b0c7805dfcfabb87c59e5697c58a2
Vladimir Sh <gratt.shvv@gmail.com>
2020-05-24 17:26:45 +0300

    Finish base table logic

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,10 +3,18 @@ const CODES = {
   Z: 90,
 }
 
-function toSells(_, col) {
-  return `
-    <div class="sell" data-col="${col}" contenteditable></div>
+function toCells(row) {
+  return function(_, col) {
+    return `
+      <div 
+        class="sell" 
+        data-type="cell" 
+        data-col="${col}"
+        data-id="${row}:${col}" 
+        contenteditable
+      ></div>
   `
+  }
 }
 
 function toColumn(col, index) {
@@ -17,7 +25,6 @@ function toColumn(col, index) {
     </div>
   `
 }
-
 
 function createRow(index, content) {
   const resize = index ? '<div class="row-resize" data-resize="row"></div>' : ''
@@ -36,27 +43,21 @@ function toChar(_, index) {
   return String.fromCharCode(CODES.A + index)
 }
 
-
 export function createTable(rowsCount = 15) {
   const colsCount = CODES.Z - CODES.A + 1
   const rows = []
-
   const cols = new Array(colsCount)
     .fill('')
     .map(toChar)
     .map(toColumn)
     .join('')
-
-  const cells = new Array(colsCount)
-    .fill('')
-    .map(toSells)
-    .join('')
-
   rows.push(createRow(null, cols))
-
-  for (let i = 0; i < rowsCount; i++) {
-    rows.push(createRow(i + 1, cells))
+  for (let row = 0; row < rowsCount; row++) {
+    const cells = new Array(colsCount)
+      .fill('')
+      .map(toCells(row))
+      .join('')
+    rows.push(createRow(row + 1, cells))
   }
-
   return rows.join('')
 }
