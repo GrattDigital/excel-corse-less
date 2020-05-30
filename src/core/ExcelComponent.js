85b8e7b17ec9da1a9e0e6399d43e44ea3dc9bef5
Vladimir Sh <gratt.shvv@gmail.com>
2020-05-30 17:55:08 +0300

    Finish redux and table logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -3,9 +3,13 @@ import {DomListener} from '@core/DomListener'
 export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners)
+
     this.name = options.name || ''
     this.emitter = options.emitter
+    this.subscribe = options.subscribe || []
+    this.store = options.store
     this.unsubscribers = []
+    // this.storeSub = null
     this.prepare()
   }
 
@@ -24,6 +28,20 @@ export class ExcelComponent extends DomListener {
     this.unsubscribers.push(unsub)
   }
 
+  $dispatch(action) {
+    this.store.dispatch(action)
+  }
+
+  // $subscribe(fn) {
+  //   this.storeSub = this.store.subscribe(fn)
+  // }
+
+  storeChanged() {}
+
+  isWatching(key) {
+    return this.subscribe.includes(key)
+  }
+
   init() {
     this.initDomListeners()
   }
@@ -31,5 +49,6 @@ export class ExcelComponent extends DomListener {
   destroy() {
     this.removeDomListeners()
     this.unsubscribers.forEach(unsub => unsub())
+    // this.storeSub.unsubscribe()
   }
 }
